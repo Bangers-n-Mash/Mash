11244d27565d8b8eb9e3ebd6c77550e9c693b4e2
Harry <niwe.harry@gmail.com>
2022-01-31 12:24:11 +0000

    Added database interaction for file upload

--- file_upload.php
+++ file_upload.php
@@ -17,42 +17,20 @@ include('includes/header.php');
         <form action="upload.php" method="post" enctype="multipart/form-data">
             Select image to upload:
             <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Upload Image" name="submit">
+            <br>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" placeholder="Title">
+            <br>
+            <label for="description">Description:</label>
+            <textarea name="description" id="description" rows="4" columns="50">File description...</textarea>
+            <br>
+            <input type="submit" value="Submit and Upload" name="submit">
+
         </form>
 
     </body>
 
     </html>
-
-    <?php
-
-    /*
-    //insert into database
-    $sql = "INSERT INTO content (content_id, content_type, title, description, likes)
-        VALUES ('2', 'picture', 'pretzel', 'I am a pretzel', '0');";
-
-    if (mysqli_query($link, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($link);
-    }
-    */
-
-    /*
-    //extract data from databse, broken
-    $sql = "SELECT  `content_id`,  `content_type`,  `title`, LEFT(`description`, 256),  `likes` FROM `mash`.`content";
-    $result = mysqli_query($link, $sql);
-        
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-          echo "title: " . $row["title"], "<br>";
-        } else {
-        echo "0 results";
-        }
-    */
-
-    ?>
 
 </div>
 
