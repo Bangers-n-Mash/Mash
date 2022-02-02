6d766e1513096b73ce81648a5bec8a636a13d507
Mike <mike.bosomworth@gmail.com>
2022-02-02 14:01:27 +0000

    Last Minute Demo Tweaks

--- includes/connect_DB.php
+++ includes/connect_DB.php
@@ -10,6 +10,5 @@ $link = mysqli_connect($serverName, $username, $passowrd, $database);
 if (!$link) {
     die('Could not connect to MySQL: ' . mysqli_connect_error());
 }
-echo "connected successfully";
 
 ?> 
