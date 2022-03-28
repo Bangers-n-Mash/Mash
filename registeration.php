47e049f46d81f76aa7a2c6211dea53d7dfae8497
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2022-03-28 21:06:52 +0100

    Registation code mostly in palce

--- registeration.php
+++ registeration.php
@@ -1,19 +1,21 @@
 <?php
 require('includes/connect_DB.php')
 
-sql = $link->prepare("INSERT INTO artAccount (username, accountPassword, forename, surname, email, account_type, usercreation_date) VALUES (?, ?, ?, ?, ?, ?);");
-$sql->bind_param("ssssi", $account_username, $account_password, $accountType, $account_email, $account_creationDate);
+// sql = $link->prepare("INSERT INTO artAccount (username, accountPassword, forename, surname, email, account_type, usercreation_date) VALUES (?, ?, ?, ?, ?, ?);");
+// $sql->bind_param("ssssi", $account_username, $account_password, $accountType, $account_email, $account_creationDate);
 $account_username = $_POST["inputUsername"];
 $account_password=$_POST["inputPassword"];
+$account_forename;
+$account_surname;
 $account_email=$_POST["inputEmail"];
 $account_bday=$_POST["birthday"];
 $account_creationDate;
 $accountType;
-$query = "INSERT into 'artAccount'(username) VALUES ()"
+$query = "INSERT into 'artAccount'(username, accountPassword, forename, surname, email, account_type, creation_date) VALUES ('$account_username', '" .md5($account_password) ."', '$account_forename', '$account_surname', '$account_email', '$accountType', '$account_creationDate')";
 
-$result =mysqli_query($con,$query)
+$result =mysqli_query($link,$query)
 
-$sql->execute();
+// $sql->execute();
 
 echo "Account Created";
 ?>
