40cef82901559a0fdbc4749b729741424b8d690f
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2022-03-29 12:45:27 +0100

    Login Details added

--- loginCode.php
+++ loginCode.php
@@ -1,23 +1,27 @@
     <?php
     require('includes/connect_DB.php');
     session_start();
-    
-    $username = mysqli_real_escape_string($link, $_POST['username']);
-    $password = mysqli_real_escape_string($link, $_POST['inputPassword']);
+    if (isset($_POST['submit'])) {  
+        $username = mysqli_real_escape_string($link, $_POST['username']);
+        $password = mysqli_real_escape_string($link, $_POST['inputPassword']);
 
-    $query = "SELECT * FROM 'artAccount' WHERE username='$username' AND password='" . md5($password) ."'";
-    $result = mysqli_query($link, $query) or die(my_sql_error());
-    $rows = mysqli_num_rows($result);
-    if ($rows == 1){
-        $_SESSION['username'] = $username;
-    }
-    else{
-        echo "<div class='form'>
-        <h3>Incorrect Username/password.</h3><br/>
+        $query = "SELECT * FROM 'artAccount' WHERE username='$username' AND password='" . md5($password) ."'";
+        $result = mysqli_query($link, $query) or die(my_sql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1){
+            $_SESSION['username'] = $username;
+        }
+        else{
+            echo "<div class='form'>
+            <h3>Incorrect Username/password.</h3><br/>
         
-        <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+            <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
 
-        </div>";
+            </div>";
+        }
+    else{
+        header("location: /login.php");
+        exit();
     }
 
     ?>
