79b62bf1877fd716782530195055ba18552597c9
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2022-03-29 12:09:17 +0100

    Debug 1

--- registeration.php
+++ registeration.php
@@ -1,8 +1,6 @@
 <?php
 require('includes/connect_DB.php')
 
-// sql = $link->prepare("INSERT INTO artAccount (username, accountPassword, forename, surname, email, account_type, usercreation_date) VALUES (?, ?, ?, ?, ?, ?);");
-// $sql->bind_param("ssssi", $account_username, $account_password, $accountType, $account_email, $account_creationDate);
 $account_username = $_POST["inputUsername"];
 $account_password=$_POST["inputPassword"];
 $account_forename=$_POST["forename"];
@@ -10,7 +8,7 @@ $account_surname=$_POST["surname"];
 $account_email=$_POST["inputEmail"];
 $account_bday=$_POST["birthday"];
 $account_creationDate = date('d/m/Y');
-$accountType;
+$accountType = 1;
 
 $query = "INSERT into 'artAccount'(username, accountPassword, forename, surname, email, account_type, creation_date, dateOfBirth) VALUES ('$account_username', '" .md5($account_password) ."', '$account_forename', '$account_surname', '$account_email', '$accountType', '$account_creationDate', '$account_bday')";
 $result =mysqli_query($link,$query)
